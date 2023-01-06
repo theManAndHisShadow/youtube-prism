2170c1f3031c060660535469b302caed70b973c6
plvs vltra <justemailpostbox@gmail.com>
2023-01-06 19:20:02 +0500

    added asyncQuerySelector func, removed 3 YouTube left menu buttons

--- js/core.js
+++ js/core.js
@@ -1,13 +1,54 @@
+const newHomeLink = "https://www.youtube.com/feed/subscriptions";
 
 function preventMainPageLoading(){
     let path = document.location.pathname;
 
     if(path === "/") {
         let url_redirect = document.createElement("a");
-        url_redirect.href = "https://www.youtube.com/feed/subscriptions";
+        url_redirect.href = newHomeLink;
 
         url_redirect.click();
     }
 }
 
+
+function asyncQuerySelector(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                resolve(document.querySelector(selector));
+                observer.disconnect();
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
+
 preventMainPageLoading();
+
+document.addEventListener("DOMContentLoaded", function(){
+    asyncQuerySelector('#start a#logo').then(logoLink => {
+        logoLink.href = newHomeLink;
+    });
+
+    asyncQuerySelector('#sections ytd-guide-entry-renderer').then(element => {
+        let leftMenu = element.parentNode;
+
+        // remove main page button from menu
+        leftMenu.children[0].remove();
+
+        // remove shorts page button from menu
+        leftMenu.children[0].remove();
+
+        // remove subs page button from menu
+        leftMenu.children[0].remove();
+    });
+});
