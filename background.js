09a325da993ad02436b773fe6f32ba520487de01
plvs vltra <justemailpostbox@gmail.com>
2023-01-12 20:48:43 +0500

    Now extension background send message to content script if url is changed for some manipulations

--- background.js
+++ background.js
@@ -1,3 +1,22 @@
 const EXTENSION_SETTINGS = {
-    
-}
+    targetSite: 'www.youtube.com'
+};
+
+
+// waits if browser history updates
+chrome.webNavigation.onHistoryStateUpdated.addListener(() => {
+    (async () => {
+        // get all active tabs
+        const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true}); 
+
+        // prepare url
+        let url = tab.url.split('//')[1].split('/')[0]; // https://www.youtube.com/feed/subscriptions
+
+        // if history changes from YT tabs
+        if(url === EXTENSION_SETTINGS.targetSite){
+            // send message
+            const response = await chrome.tabs.sendMessage(tab.id, {url: tab.url});
+            // do something with response here, not outside the function
+        }
+    })();
+});
