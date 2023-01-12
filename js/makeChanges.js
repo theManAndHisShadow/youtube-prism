fff4bde669452077d1b38cc4c548c50c1a4ca44c
plvs vltra <justemailpostbox@gmail.com>
2023-01-12 14:11:09 +0500

    makeChanges.js rewrited using new core methods

--- js/makeChanges.js
+++ js/makeChanges.js
@@ -3,52 +3,52 @@
 Prism.atPage('main').redirectTo('subscriptions');
 
 document.addEventListener("DOMContentLoaded", function(){
-    Prism.atPage('any').execute(() => {});
+
+    Prism.atPage('any').execute(() => {
+        Prism.findElement('#start a#logo').modify(logoLink => {
+            remakeLogoButton(logoLink.parentNode);
+        });
+
+        // For cases, where YT logo button also placed in collapsed left menu. F
+        // or example, on watch page
+        Prism.findElement('#contentContainer a#logo').modify(logoLink => {
+            remakeLogoButton(logoLink.parentNode);
+        });
+
+        // Removing first three elements from full size left menu
+        Prism.findElement('#sections #items').modify(leftMenuBlock => {
+            // remove main page button from menu
+            leftMenuBlock.children[0].remove();
+
+            // remove shorts page button from menu
+            leftMenuBlock.children[0].remove();
+        });
+
+        // removing navigator block
+        Prism.findElement('#sections ytd-guide-section-renderer:nth-child(3)').modify(navigatorBlock => {
+            navigatorBlock.remove();
+        });
+
+        // removing other features bock
+        Prism.findElement('#sections ytd-guide-section-renderer:nth-child(4)').modify(otherFeaturesBlock => {
+            otherFeaturesBlock.remove();
+        });
+        
+        // Removing mini menu
+        Prism.findElement('ytd-mini-guide-renderer[role="navigation"] #items').modify(miniMenu => {
+            miniMenu.parentNode.remove();
+        });
+    });
     
     Prism.atPage('subscriptions').execute(() => {});
     
-    Prism.atPage('watch').execute(() => {});
-    
-    // TODO: Refactor this code block
-    // This solution is very unoptimal, cause make node.. 
-    // ..clone operations
-    asyncQuerySelector('#start a#logo').then(logoLink => {
-        // YouTube logo button 
-        remakeLogoButton(logoLink.parentNode);
+    Prism.atPage('watch').execute(() => {
+        // Removing the most distractive feature - realted videos >:}
+        Prism.findElement('#secondary #related').modify(related => {
+            related.remove();
+        });
     });
 
-    // For cases, where YT logo button also placed in collapsed left menu. F
-    // or example, on watch page
-    asyncQuerySelector('#contentContainer a#logo').then(logoLink => {
-        remakeLogoButton(logoLink.parentNode);
-    });
-
-    // Removing first three elements from full size left menu
-    asyncQuerySelector('#sections #items').then(leftMenuBlock => {
-        // remove main page button from menu
-        leftMenuBlock.children[0].remove();
-
-        // remove shorts page button from menu
-        leftMenuBlock.children[0].remove();
-    });
-
-    asyncQuerySelector('#sections ytd-guide-section-renderer:nth-child(3)').then(navigatorBlock => {
-        navigatorBlock.remove();
-    });
-
-    asyncQuerySelector('#sections ytd-guide-section-renderer:nth-child(4)').then(otherFeaturesBlock => {
-        otherFeaturesBlock.remove();
-    });
-    
-    // Removing mini menu
-    asyncQuerySelector('ytd-mini-guide-renderer[role="navigation"] #items').then(miniMenu => {
-        miniMenu.parentNode.remove();
-    });
-
-    // Removing the most distractive feature - realted videos >:}
-    asyncQuerySelector('#secondary #related').then(related => {
-        related.remove();
-    });
 
     showOrHideNextVideoButton();
 });
